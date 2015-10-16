482d93bb6921f712c67420e6bf47423172e05014
Lam H. Dao <daohailam@yahoo.com>
2015-10-16 08:54:40 -0400

    Added description and license notification

--- src/box.cpp
+++ src/box.cpp
@@ -1,3 +1,15 @@
+//--------------------------------------------------------------------------
+// box.cpp - Bounding box intersection implemented using SSE
+//--------------------------------------------------------------------------
+// Author: Lam H. Dao <daohailam(at)yahoo(dot)com>
+//--------------------------------------------------------------------------
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//--------------------------------------------------------------------------
 #include "box.h"
 //------------------------------------------------------------------------
 #ifndef min
--- src/vector3.h
+++ src/vector3.h
@@ -1,3 +1,15 @@
+//--------------------------------------------------------------------------
+// vector3.h - Vector manipulation/algorithm implemented using SSE
+//--------------------------------------------------------------------------
+// Author: Lam H. Dao <daohailam(at)yahoo(dot)com>
+//--------------------------------------------------------------------------
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//--------------------------------------------------------------------------
 #ifndef __VECTOR3_H_
 #define __VECTOR3_H_
 //------------------------------------------------------------------------
